81240be774c50e16cb0d7b029d51a69d236eb30c
Padde <patrick_kloes@arcor.de>
2017-02-14 20:45:22 +0100

    rename script bugfix

--- rename.py
+++ rename.py
@@ -4,6 +4,6 @@ import sys
 files = glob.glob(sys.argv[1] + os.path.sep + 'win_*.dll')
 files.extend(glob.glob(sys.argv[1] + os.path.sep + 'mac_*.dylib'))
 for file in files:
-    os.rename(file, sys.argv[1] + os.path.sep + format(file.rsplit('_')[1]))
+    #os.rename(file, sys.argv[1] + os.path.sep + format(file.rsplit('_')[1]))
     print(file)
     print(sys.argv[1] + os.path.sep + format(file.rsplit('_')[1]))
